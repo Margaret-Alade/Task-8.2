3be4780ce9178ceb2d7bbb2da90c4d91a158c12e
Margaret Alade <alade.margaret@yandex.ru>
2023-11-03 06:28:35 +0300

    Initial Commit

---
+++ Task-8.2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task-8.2
+//
+//  Created by Маргарет  on 03.11.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
